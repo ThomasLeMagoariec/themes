d005c20bdd57e3acdc7581c22bff6b622e2e4783
thomas <thomas.lemagoariec@outlook.com>
2026-09-01 00:20:48 +0200

    theme

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,33 +1,39 @@
 { config, lib, pkgs, ... }:
 let
-  cfg = config.programs.whatever;
-
-  themes = {
-    rose-pine = ''
-      background = #191724
-      foreground = #e0def4
-    '';
-    gruvbox = ''
-      background = #282828
-      foreground = #ebdbb2
-    '';
-    nord = ''
-      background = #2e3440
-      foreground = #d8dee9
-    '';
-  };
+  c = config.theme.colors;
 in
 {
-  options.programs.whatever = {
-    enable = lib.mkEnableOption "whatever";
-    theme = lib.mkOption {
-      type = lib.types.enum [ "rose-pine" "gruvbox" "nord" ];
-      default = "rose-pine";
+  config = lib.mkIf config.programs.kitty.enable {
+    programs.kitty.settings = {
+      foreground              = c.foreground;
+      background               = c.background;
+      selection_foreground     = c.selectionForeground;
+      selection_background     = c.selectionBackground;
+
+      cursor                   = c.cursor;
+      cursor_text_color        = c.cursorText;
+
+      url_color                 = c.url;
+
+      active_border_color       = c.activeBorder;
+      inactive_border_color     = c.inactiveBorder;
+      bell_border_color         = c.bellBorder;
+
+      wayland_titlebar_color    = "system";
+      macos_titlebar_color      = "system";
+
+      active_tab_foreground     = c.activeTabForeground;
+      active_tab_background     = c.activeTabBackground;
+      inactive_tab_foreground   = c.inactiveTabForeground;
+      inactive_tab_background   = c.inactiveTabBackground;
+      tab_bar_background        = c.tabBarBackground;
+
+      mark1_foreground = c.mark1Foreground;
+      mark1_background = c.mark1Background;
+      mark2_foreground = c.mark2Foreground;
+      mark2_background = c.mark2Background;
+      mark3_foreground = c.mark3Foreground;
+      mark3_background = c.mark3Background;
     };
-  };
-
-  config = lib.mkIf cfg.enable {
-
-    home.file.".whateverrc".text = themes.${cfg.theme};
   };
 }
